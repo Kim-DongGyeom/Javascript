6500e42927f5ba25dde03ced495d7905f962f778
Kim-DongGyeom <kdg980817@yahoo.co.jp>
2026-02-17 23:53:04 +0900

    todo Create sec

--- todolist/clock.js
+++ todolist/clock.js
@@ -1,24 +1,23 @@
 (function () {
-  /**
-   * 曜日のリスト
-   * []には現在の曜日を数字で表す dayOfWeek　を反映
-   * ex) 0 => 日, 1 => 月 ...
-   */
-  const dayOfWeekList = ['日', '月', '火', '水', '木', '金', '土'];
-
   // DOM
   const clock = document.querySelector('.clock');
 
-  // utill
-  /**
-   * itemが 10 未満の場合、 0をつける
-   * ex) 1 => 01, 2 => 02 ...
-   */
-  const setNum = item => String(item).padStart(2, '0');
-
   // main初期化
-  const getTime = () => {};
-  const init = () => {
+  const getTime = () => {
+    /**
+     * 曜日のリスト
+     * []には現在の曜日を数字で表す dayOfWeek　を反映
+     * ex) 0 => 日, 1 => 月 ...
+     */
+    const dayOfWeekList = ['日', '月', '火', '水', '木', '金', '土'];
+
+    // utill
+    /**
+     * itemが 10 未満の場合、 0をつける
+     * ex) 1 => 01, 2 => 02 ...
+     */
+    const setNum = item => String(item).padStart(2, '0');
+
     const date = new Date();
 
     const year = date.getFullYear();
@@ -27,12 +26,17 @@
      */
     const month = setNum(date.getMonth() + 1);
     const day = setNum(date.getDate());
-    const dayOfWeek = date.getDay();
+    const dayOfWeek = dayOfWeekList[date.getDay()];
+    const time = date.toString().slice(16, 24);
 
-    console.log(year, month, day, dayOfWeekList[dayOfWeek]);
-    console.log(clock);
+    clock.textContent = `${year}年 ${month}月 ${day}日 (${dayOfWeek}) ${time}`;
+    // console.log(clock);
   };
-  ㅏ;
+  const init = () => {
+    setInterval(() => {
+      getTime();
+    }, 1000);
+  };
 
   init();
 })();
