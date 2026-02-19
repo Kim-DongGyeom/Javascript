c93f68e4d527299fcbee347e4ccfa3186d46ab45
Kim-DongGyeom <kdg980817@yahoo.co.jp>
2026-02-20 00:02:55 +0900

    main.js update

--- todolist/src/dumpData.js
+++ todolist/src/dumpData.js
@@ -1,6 +1,6 @@
 const a = document.querySelector('.clock').innerText;
 
-const arr = [
+const toDoList = [
   { id: 0, state: true, content: '日本語', time: a },
   { id: 1, state: false, content: 'javaScript', time: a },
   { id: 2, state: false, content: 'PHP', time: a },
--- todolist/src/main.js
+++ todolist/src/main.js
@@ -1,24 +1,53 @@
 (function main() {
   const TODOLIST = 'toDoList';
+
+  // DOM
   const constentMenu = document.querySelector('.constent_menu');
   const toDoInput = document.querySelector('.constent_menu');
   const toDoAddBtn = document.querySelector('.toDo_addBtn');
   const toDoDelBtn = document.querySelector('.toDo_delBtn');
   const toDoList = document.querySelector('.toDo_list');
 
+  let data;
   const loadTodo = () => {
-    const data = JSON.parse(localStorage.getItem(TODOLIST));
-    // console.log(data);
+    // データをlocalstrageから取得
+    data = JSON.parse(localStorage.getItem(TODOLIST));
+    console.log(data);
+
+    /**
+     * item: toDoListの要素
+     * idx:index
+     */
     data.forEach((item, idx) => {
-      item['id'] = idx + 1;
+      item.id = idx;
       getToDoList(item);
-      //   console.log(item);
     });
+  };
+  const del = id => {
+    console.log(data[id]);
+    document.getElementById(id).remove();
+  };
+
+  const toggle = id => {
+    // idとindexが異なる可能性があるため、ループで対象のデータを探す
+    /*
+    // for文の場合
+    for (i = 0; i < data; i++) {
+      if (data[i]['id'] === id) {
+        data[i]['state'] = !data[i]['state'];
+        break;
+      }
+    }
+    */
+    const item = data.find(todo => todo.id === id);
+
+    if (item) {
+      item.state = !item.state;
+    }
   };
 
   const getToDoList = item => {
     const { id, state, content, time } = item;
-    console.log(state);
 
     const div = document.createElement('div');
     div.id = id;
@@ -27,7 +56,10 @@
     const checkBox = document.createElement('input');
     checkBox.className = 'Todo_List_State';
     checkBox.type = 'checkbox';
-    checkBox.textContent = state;
+    checkBox.checked = state;
+    checkBox.addEventListener('click', () => {
+      toggle(id);
+    });
     div.append(checkBox);
 
     const textDiv = document.createElement('div');
@@ -41,9 +73,13 @@
     div.append(TimeDiv);
 
     const delBTN = document.createElement('button');
-    delBTN.className = 'ToDo_list_btn';
+    delBTN.className = 'ToDo_list_delBtn';
     delBTN.type = 'button';
     delBTN.textContent = '削除';
+    delBTN.addEventListener('click', () => {
+      del(id);
+    });
+
     div.append(delBTN);
 
     toDoList.append(div);
