fb31bc054db855ab507f545e6380414dadfaf320
Kim-DongGyeom <kdg980817@yahoo.co.jp>
2026-03-10 00:08:49 +0900

    react-app blog

---
+++ react/blog/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let post = '西鉄天神高速バスターミナル';
+  return (
+    <div className='App'>
+      <div className='black-nav'>
+        <h4>ReactApp-Blog</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
